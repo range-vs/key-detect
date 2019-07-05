de1b7bac492b979c432b27f52512c135a322a269
LAPTOP-GOG9SDSJ\admin_test <trubnicov_ivan1996@mail.ru>
2019-07-05 21:13:04 +0300

    + final test uncoding to utf-8

--- hook_key/EventAltN.cpp
+++ hook_key/EventAltN.cpp
@@ -5,7 +5,7 @@ int EventAltN::run(SmartEventData gh)
 	if (GetKeyState(160) & 0x80 || GetKeyState(161) & 0x80)
 	{
 		data = L"Нажата комбинация alt + N\n";
-		data += gh->isAltN() ? L"\nПравый enter вновь соответствует своему значению\n" : L"\nПравый enter изменил своё значение на 'н'\n";
+		data += gh->isAltN() ? L"\nПравый enter вновь соответствует своему значению\n" : L"\nПравый enter изменил своё значение на 'н'\n"; 
 		gh->switchAltN();
 		time_t seconds = time(NULL);
 		tm timeinfo;
